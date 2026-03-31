368571a8c73afa9cd7272c6712df93977719f057
Yara <3122107374@qq.com>
2026-03-31 23:27:33 +0800

    Update stock_analysis2.py

--- stock_analysis2.py
+++ stock_analysis2.py
@@ -3,11 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from matplotlib.pyplot import figure
-
-
 plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
-
 
 # 读取 CSV，将 date 转为 datetime 并设为索引。
 # 计算日收益率
@@ -28,7 +25,6 @@ print(df.head())
 # 手动实现 20 日移动均线 MA20。
 # 计算 价格偏离率：
 # 找出 deviation 绝对值 > 2% 的日期，统计天数
-
 df['ma5'] = df['close'].rolling(5).mean()
 df['ma20'] = df['close'].rolling(20).mean()
 df['deviation'] = (df['close']-df['ma5'])/df['ma5']
@@ -38,8 +34,6 @@ dev_days = len(df_dev)
 print(df_dev[['close','ma5','deviation']].round(4).to_string())
 print('deviation 绝对值 > 2% 的天数',dev_days)
 
-
-
 # 波动率与风险指标
 # 计算年化波动率（交易日 252 天）：
 # 计算滚动 20 日波动率（rolling std）。
@@ -48,20 +42,18 @@ daily_vol = df['return'].std()
 annual_vol = daily_vol*np.sqrt(252)
 print(annual_vol.round(4))
 df['vol_20'] = df['return'].rolling(20).std().round(4)
-# fig, ax1 = plt.subplots(figsize=(18,10))
-# ax1.plot(df.index, df['close'],color='blue',linewidth=1.8,label='收盘价')
-# ax1.set_ylabel('收盘价',color='blue',fontsize=22)
-# ax1.tick_params(axis='y', labelcolor='blue')
-# ax2=ax1.twinx()
-# ax2.plot(df.index,df['vol_20'],color='red',linewidth=1.6,label='滚动 20 日波动率')
-# ax2.set_ylabel('滚动 20 日波动率',color='red',fontsize=22)
-# ax2.tick_params(axis='y', labelcolor='red')
-# plt.title('收盘价 + 滚动 20 日波动率（双轴图）',fontsize=26)
-# plt.grid(True)
-# plt.tight_layout()
-# plt.show()
-
-
+fig, ax1 = plt.subplots(figsize=(18,10))
+ax1.plot(df.index, df['close'],color='blue',linewidth=1.8,label='收盘价')
+ax1.set_ylabel('收盘价',color='blue',fontsize=22)
+ax1.tick_params(axis='y', labelcolor='blue')
+ax2=ax1.twinx()
+ax2.plot(df.index,df['vol_20'],color='red',linewidth=1.6,label='滚动 20 日波动率')
+ax2.set_ylabel('滚动 20 日波动率',color='red',fontsize=22)
+ax2.tick_params(axis='y', labelcolor='red')
+plt.title('收盘价 + 滚动 20 日波动率（双轴图）',fontsize=26)
+plt.grid(True)
+plt.tight_layout()
+plt.show()
 
 # 量价关系
 # 计算 量价相关系数（volume 与 daily return）。
@@ -73,25 +65,22 @@ x = df['volume']
 y = df['return']
 z = np.polyfit(x, y, 1)
 p = np.poly1d(z)
-# plt.figure(figsize=(12, 6))
-# plt.scatter(x, y, s=20, alpha=0.6, color='#5D737E')
-# plt.plot(x, p(x), "r--", linewidth=1.5, label=f'拟合线 y={z[0]:.2e}x+{z[1]:.4f}')
-# plt.xlabel('成交量')
-# plt.ylabel('日收益率')
-# plt.title(f'量价散点图 | 相关系数={corr:.4f}', fontsize=20)
-# plt.legend()
-# plt.grid(alpha=0.3)
-# plt.tight_layout()
-# plt.show()
-
-
+plt.figure(figsize=(12, 6))
+plt.scatter(x, y, s=20, alpha=0.6, color='#5D737E')
+plt.plot(x, p(x), "r--", linewidth=1.5, label=f'拟合线 y={z[0]:.2e}x+{z[1]:.4f}')
+plt.xlabel('成交量')
+plt.ylabel('日收益率')
+plt.title(f'量价散点图 | 相关系数={corr:.4f}', fontsize=20)
+plt.legend()
+plt.grid(alpha=0.3)
+plt.tight_layout()
+plt.show()
 
 # 均线策略信号
 # 金叉：MA5 上穿 MA20 → 信号 = 1
 # 死叉：MA5 下穿 MA20 → 信号 =-1
 # 统计全段金叉、死叉各出现几次
 # 画出 MA5、MA20、Close 三线图
-
 df['ma_diff'] = df['ma5'] - df['ma20']
 df['signal'] = 0
 df.loc[(df['ma_diff'] > 0) & (df['ma_diff'].shift(1) <= 0), 'signal'] = 1  # 金叉
@@ -100,21 +89,19 @@ golden = df[df['signal'] == 1].shape[0]
 dead = df[df['signal'] == -1].shape[0]
 print('金叉次数',golden)
 print('死叉次数',dead)
-# plt.figure(figsize=[14,16])
-# plt.plot(df['ma5'], label='ma5',linewidth=1.8)
-# plt.plot(df['ma20'], label='ma20',linewidth=1.8)
-# plt.plot(df['close'], label='收盘价',linewidth=2)
-# plt.scatter(df[df['signal']==1].index, df[df['signal']==1]['ma5'], marker='^', color='red', s=60, label='金叉')
-# plt.scatter(df[df['signal']==-1].index, df[df['signal']==-1]['ma5'], marker='v', color='green', s=60, label='死叉')
-# plt.title('均线金叉死叉', fontsize=14)
-# plt.legend()
-# plt.grid(alpha=0.4)
-# plt.tight_layout()
-# plt.show()
+plt.figure(figsize=[14,16])
+plt.plot(df['ma5'], label='ma5',linewidth=1.8)
+plt.plot(df['ma20'], label='ma20',linewidth=1.8)
+plt.plot(df['close'], label='收盘价',linewidth=2)
+plt.scatter(df[df['signal']==1].index, df[df['signal']==1]['ma5'], marker='^', color='red', s=60, label='金叉')
+plt.scatter(df[df['signal']==-1].index, df[df['signal']==-1]['ma5'], marker='v', color='green', s=60, label='死叉')
+plt.title('均线金叉死叉', fontsize=14)
+plt.legend()
+plt.grid(alpha=0.4)
+plt.tight_layout()
+plt.show()
 
-
-
-# 任务 6：最大回撤
+# 最大回撤
 # 计算累计收益曲线：cumprod(1 + return)
 # 计算滚动最大值（高位线）。
 # 画出：累计收益 + 回撤曲线（上下子图)
